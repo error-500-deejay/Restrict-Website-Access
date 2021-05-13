1809922170d33e8b968503a0554d4d9badae5179
error-500-deejay <38355224+error-500-deejay@users.noreply.github.com>
2021-05-13 10:43:51 +0530

    move items

--- extension-code/content.js
+++ extension-code/content.js
@@ -6,9 +6,9 @@ chrome.runtime.sendMessage(
     console.log(response);
     datajson = response.webListData;
     console.log(datajson);
-    //let extensionurl = chrome.runtime.getURL("bg.png");
-    let extensionurl =
-      "https://lh3.googleusercontent.com/W6vKpQRJlXBt2nC7bzz-nkz72ig9daz-GRCuznyGylGZgpZz3qNuj_6pVqWHHqxFffI4rZIT27Bmjw=w1920-h902-rw";
+    let extensionurl = chrome.runtime.getURL("bg.PNG");
+    //let extensionurl =
+    //"https://lh3.googleusercontent.com/W6vKpQRJlXBt2nC7bzz-nkz72ig9daz-GRCuznyGylGZgpZz3qNuj_6pVqWHHqxFffI4rZIT27Bmjw=w1920-h902-rw";
     console.log(extensionurl);
     if (datajson.action == "block") {
       if (
